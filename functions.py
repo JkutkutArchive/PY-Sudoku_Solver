0834762cf16c91a97d03c20dba4bccad0634f3ce
Jkutkut <jorgeregonzalez@gmail.com>
2020-05-26 23:34:19 +0200

    added functions from py-Tools

--- functions.py
+++ functions.py
@@ -10,25 +10,6 @@ sol = [
     [1, 9, 6, 7, 5, 3, 2, 8, 4]
 ]
 
-
-def printArray(arr, *delimeter):
-    print(arrayToString(arr, *delimeter))
-
-def arrayToString(arr, *delimeter):
-    d = delimeter if delimeter else "\n"
-    end = ""
-    s = "["
-    for subArr in arr:
-        if isinstance(subArr, list):
-            if isinstance(subArr[0], list):
-                end = "\n"
-            s = s + arrayToString(subArr, delimeter)
-        else:
-            s = s + str(subArr) # This is an element of the array
-            d = ","
-        s = s + ((d + " " if True else "") + end)
-    return s[0:-(1 + len(d))] + "]"
-
 def printSudoku(arr):
     print(*["  Y "] + [str(i) + " " for i in range(3)] + ["  "] + [str(3+i) + " " for i in range(3)] + ["  "] + [str(6+i) + " " for i in range(3)], sep = "")
     print(*["X +"] + ["-" for i in range(23)] + ["+"], sep = "");#start
@@ -51,11 +32,46 @@ def printSudoku(arr):
 
     print(*["  +"] + ["-" for i in range(23)] + ["+"], sep = "");#end
 
-def checkSudokuSolution(grid, solution):
-    for i in range(9):
-        for j in range(9):
-            if grid[i][j].getValue() != solution[i][j]:
+def sudokuSolution(data, solutions):
+    for x in range(9):
+        for y in range(9):
+            if data[x][y] == 0:
+                for val in range(1,10):
+                    valid = True
+                    for i in range(9):
+                        xIndex = (x // 3) * 3
+                        yIndex = (y // 3) * 3
+                        if data[x][i] == val or data[i][y] == val or data[xIndex + (i // 3)][yIndex + (i % 3)] == val:
+                            valid = False
+                            break
+                    if valid:
+                        data[x][y] = val
+                        sudokuSolution(data, solutions)
+                        data[x][y] = 0 #if here, the path wasn't good => undo move
+                return
+    ## if here, solution founded
+    solutions.append([[ele for ele in row] for row in data])
+
+# Check if the given sudoku is a valid solution
+def checkSol(arr):
+    for i in range(0, 9, 3):#3 by 3:
+        for j in range(0, 9, 3):
+            suma = 0
+            ele = []
+            for k in range(3):
+                for l in range(3):
+                    suma = suma + arr[k + i][l + j]
+                    ele = ele + [arr[k + i][l + j]]
+            if(suma != 45):
+                # pError(zone="3b3", i=i, j=j, k=k, l=l, ele=ele)
                 return False
+    for l in range(9):#lines:
+        solC = 0
+        for i in range(9):
+            solC = solC + arr[i][l]
+        if sum(arr[l]) != 45 or solC != 45:
+            # pError(zone="lines", i=i, l=l)
+            return False
     return True
 
 ## Prints the status of the arguments (errors)
--- thingsToDo.md
+++ thingsToDo.md
@@ -1,12 +1,11 @@
 ## main:
-- Rows and col doc can be together
+- bsic Rows and col logic can be together
 - handle Errors
-- Error with cols and rows
 - Difficult sudokus
 
 ## functions:
 - revise Eq method
-- at addData, no need to use array
+- at addData, no need to use array as input
 - at AddData, handle erros
 - at AddData, use Set and update already created elemets
 
