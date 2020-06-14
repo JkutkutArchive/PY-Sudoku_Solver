ec9b23505476f15090c0f24cbabfe0bb043c1d12
Jkutkut <jorgeregonzalez@gmail.com>
2020-06-14 21:35:21 +0200

    Ready to work on XY-Wing

--- main.py
+++ main.py
@@ -78,16 +78,28 @@ grid = [[tool.Cell(x, y) for y in range(9)] for x in range(9)]
 #     [0, 0, 0, 7, 0, 0, 0, 6, 3]
 # ]
 
-data = [ # X-Wing
-    [0, 0, 3, 8, 0, 0, 5, 1, 0],
-    [0, 0, 8, 7, 0, 0, 9, 3, 0],
-    [1, 0, 0, 3, 0, 5, 7, 2, 8],
-    [0, 0, 0, 2, 0, 0, 8, 4, 9],
-    [8, 0, 1, 9, 0, 6, 2, 5, 7],
-    [0, 0, 0, 5, 0, 0, 1, 6, 3],
-    [9, 6, 4, 1, 2, 7, 3, 8, 5],
-    [3, 8, 2, 6, 5, 9, 4, 7, 1],
-    [0, 1, 0, 4, 0, 0, 6, 9, 2]
+# data = [ # X-Wing
+#     [0, 0, 3, 8, 0, 0, 5, 1, 0],
+#     [0, 0, 8, 7, 0, 0, 9, 3, 0],
+#     [1, 0, 0, 3, 0, 5, 7, 2, 8],
+#     [0, 0, 0, 2, 0, 0, 8, 4, 9],
+#     [8, 0, 1, 9, 0, 6, 2, 5, 7],
+#     [0, 0, 0, 5, 0, 0, 1, 6, 3],
+#     [9, 6, 4, 1, 2, 7, 3, 8, 5],
+#     [3, 8, 2, 6, 5, 9, 4, 7, 1],
+#     [0, 1, 0, 4, 0, 0, 6, 9, 2]
+# ]
+
+data = [ # XY-Wing
+    [8, 0, 0, 3, 6, 0, 9, 0, 0],
+    [0, 0, 9, 0, 1, 0, 8, 6, 3],
+    [0, 6, 3, 0, 8, 9, 0, 0, 5],
+    [9, 2, 4, 6, 7, 3, 1, 5, 8],
+    [3, 8, 6, 9, 5, 1, 7, 2, 4],
+    [5, 7, 1, 8, 2, 4, 3, 9, 6],
+    [4, 3, 2, 1, 9, 6, 5, 8, 7],
+    [6, 9, 8, 5, 3, 7, 0, 0, 0],
+    [0, 0, 0, 2, 4, 8, 6, 3, 9]
 ]
 
 # data = [ # expert
@@ -394,6 +406,16 @@ while gameRunning:
                             print(c.getPos())
                             c.addData("X-Wing " + ["row", "col"][t], p1, p2, value) # Add Data
                             c.removePosVal(value) # Remove this value from possible values
+
+    # ----------    XY Wing   ----------
+    # Row:
+    #   - Row pair (c1, c2) with a value v1
+    #       - No multiple pairs on this pair
+    #       - len(posVal) on both is 2
+    #   - One of the members (c1) of the pair have another relation on a 3by3 pair: (c1, c3) with value v2
+    #       - len(c3.posVal) = 2
+    #       - both c2 and c3 has as second posVal v2
+    #   If all OK: cells at (c3.x, T),, T = Sector(c2).y's can not be v2
 
     response = input("Continue?")
     # response = ""
