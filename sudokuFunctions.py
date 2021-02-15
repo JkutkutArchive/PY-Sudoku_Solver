a73946f42d8ff5a71cbeecad2ba69b43559a57af
jkutkut <jorgeregonzalez@gmail.com>
2021-02-15 23:23:46 +0100

    functions from functions.py moved here. Code commented and organiced

---
+++ sudokuFunctions.py
@@ -0,0 +1,96 @@
+def printSudoku(arr):
+    '''
+    Prints with sudoku format the 9x9 list given.
+
+    Given a 9x9 list of integers, prints on console the ASCII representation on the format used by this code.
+
+    - arr (list): 9x9 list with integers
+
+    Please note that the axis are inverted compared with the programming convention.
+    '''
+
+    print(*["  Y "] + [str(i) + " " for i in range(3)] + ["  "] + [str(3+i) + " " for i in range(3)] + ["  "] + [str(6+i) + " " for i in range(3)], sep = "")
+    print(*["X +"] + ["-" for i in range(23)] + ["+"], sep = "")#start
+
+    for i in range(3): #rows
+        t = [str(i) + " |"] + arr[i][0:3] + ["|"] + arr[i][3:6] + ["|"] + arr[i][6:9] + ["|"]
+        print(*t, sep = " ")
+
+    print(*["  "]+["".join(["+"] + ["-" for i in range(7)]) for j in range(3)] + ["+"], sep = "")#3 by 3 separators
+
+    for i in range(3, 6): #rows
+        t = [str(i) + " |"] + arr[i][0:3] + ["|"] + arr[i][3:6] + ["|"] + arr[i][6:9] + ["|"]
+        print(*t, sep = " ")
+
+    print(*["  "]+["".join(["+"] + ["-" for i in range(7)]) for j in range(3)] + ["+"], sep = "")#3 by 3 separators
+
+    for i in range(6, 9): #rows
+        t = [str(i) + " |"] + arr[i][0:3] + ["|"] + arr[i][3:6] + ["|"] + arr[i][6:9] + ["|"]
+        print(*t, sep = " ")
+
+    print(*["  +"] + ["-" for i in range(23)] + ["+"], sep = "")#end
+
+
+def checkSol(arr):
+    '''
+    Check if the given sudoku is a valid solution
+
+    Given a 9x9 Cell list, check if it forms a valid sudoku.
+
+    - arr (list): 9x9 Cell list
+
+    Returns:
+    boolean: whenever the input forms a valid sudoku
+
+    Note: See the Cell class for more information about who this function works.
+    '''
+    for i in range(0, 9, 3):#3 by 3:
+        for j in range(0, 9, 3):
+            suma = 0
+            ele = []
+            for k in range(3):
+                for l in range(3):
+                    suma = suma + arr[k + i][l + j].getValue()
+                    ele = ele + [arr[k + i][l + j].getValue()]
+            if(suma != 45):
+                return False
+    for l in range(9):#lines:
+        solCc = 0
+        solCr = 0
+        for i in range(9):
+            solCc = solCc + arr[i][l].getValue()
+            solCr = solCr + arr[l][i].getValue()
+        if solCc != 45 or solCr != 45:
+            return False
+    return True
+
+
+def sudokuSolution(data, solutions):
+    '''
+    Gets the possible solution(s) of the given sudoku.
+
+    Given a 9x9 integer list, all the possible solutions of the input are found using a classic implementation of a recursive algorithm.
+
+    - data (list): 9x9 integer list with the data of the current state of the sudoku
+    - solutions (list) Nx9x9 with the solutions found. This works as a "output".
+    '''
+    for x in range(9): # For each colum
+        for y in range(9): # For each row
+            if data[x][y] == 0: # If empty cell
+                for val in range(1,10): # For each possible value
+                    valid = True # If the current value (val) is viable to be the correct value of the cell(x, y)
+                    for i in range(9): # for each neighbour cell (row, col or 3by3)
+                        xIndex = (x // 3) * 3 # X 3by3
+                        yIndex = (y // 3) * 3 # Y 3by3
+                        if data[x][i] == val or data[i][y] == val or data[xIndex + (i // 3)][yIndex + (i % 3)] == val:
+                            # If neighbour already has the value val, this value can not be on this cell => val not valid
+                            valid = False
+                            break
+                    if valid: # If the value val may be correct
+                        data[x][y] = val # Try to solve the sudoku using this value as correct
+                        sudokuSolution(data, solutions)
+                        data[x][y] = 0 # if here, the path wasn't good => undo move
+                return # If here, all posible valid combinations have been tested => end execution
+    
+    ## if here, solution founded
+    solutions.append([[ele for ele in row] for row in data]) # Add the current solution to the solution list
