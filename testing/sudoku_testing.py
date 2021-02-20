f4a418fa76c8dde1bc2e341930fc24f8c83b205c
jkutkut <jorgeregonzalez@gmail.com>
2021-02-20 18:46:49 +0100

    testing the new way to create a sudoku object

--- testing/sudoku_testing.py
+++ testing/sudoku_testing.py
@@ -15,6 +15,20 @@ class TestStringMethods(unittest.TestCase):
     def setUp(self):
         self.sudoku = sudoku.Sudoku()
 
+    def test_initClass(self):
+        sud1 = sudoku.Sudoku()
+        board = input.swordfish()
+        sud2 = sudoku.Sudoku(board)
+
+        sud1Board = sud1.toList()
+        sud2Board = sud2.toList()
+
+        for r in range(9):
+            for c in range(9):
+                self.assertEquals(sud1Board[r][c].getValue(), 0)
+                self.assertEquals(sud2Board[r][c].getValue(), board[r][c])
+
+
     def test_fillSudoku(self):
         testSudoku = sudoku.Sudoku()
         boardTest = testSudoku.toList()
@@ -27,27 +41,11 @@ class TestStringMethods(unittest.TestCase):
                 self.assertEquals(boardTest[r][c].getValue(), 0)
                 self.assertEquals(board[r][c].getValue(), spected[r][c])
 
-        
-
     def test_printSudokuTest(self):
         self.sudoku.fillBoard(input.easy())
         self.maxDiff = None # To compare the whole output
 
-        espected = \
-"  C 0 1 2   3 4 5   6 7 8 \n\
-R +-------+-------+-------+\n\
-0 | 9 8 4 | 0 3 1 | 0 7 2 |\n\
-1 | 6 1 0 | 0 0 7 | 0 0 0 |\n\
-2 | 2 5 7 | 0 0 9 | 8 0 0 |\n\
-  +-------+-------+-------+\n\
-3 | 3 0 0 | 0 6 0 | 0 1 0 |\n\
-4 | 0 0 0 | 3 7 0 | 9 2 0 |\n\
-5 | 0 0 9 | 0 0 5 | 0 0 0 |\n\
-  +-------+-------+-------+\n\
-6 | 0 3 0 | 0 0 6 | 0 0 0 |\n\
-7 | 0 4 5 | 0 1 8 | 0 9 6 |\n\
-8 | 1 9 6 | 7 0 0 | 2 8 0 |\n\
-  +-------+-------+-------+"
+        espected = "  C 0 1 2   3 4 5   6 7 8 \nR +-------+-------+-------+\n0 | 9 8 4 | 0 3 1 | 0 7 2 |\n1 | 6 1 0 | 0 0 7 | 0 0 0 |\n2 | 2 5 7 | 0 0 9 | 8 0 0 |\n  +-------+-------+-------+\n3 | 3 0 0 | 0 6 0 | 0 1 0 |\n4 | 0 0 0 | 3 7 0 | 9 2 0 |\n5 | 0 0 9 | 0 0 5 | 0 0 0 |\n  +-------+-------+-------+\n6 | 0 3 0 | 0 0 6 | 0 0 0 |\n7 | 0 4 5 | 0 1 8 | 0 9 6 |\n8 | 1 9 6 | 7 0 0 | 2 8 0 |\n  +-------+-------+-------+"
 
         self.assertEqual(self.sudoku.print(returnAsString=True), espected)
 
@@ -56,9 +54,9 @@ R +-------+-------+-------+\n\
         self.assertIsNone(self.sudoku.print())
 
 if __name__ == '__main__':
-  print("Testing...")
+    print("Testing...")
 
-  logging.basicConfig( stream=sys.stderr )
-  logging.getLogger( "TestStringMethods.printSudokuTest" ).setLevel( logging.DEBUG )
-  
-  unittest.main()
+    logging.basicConfig( stream=sys.stderr )
+    logging.getLogger( "TestStringMethods.printSudokuTest" ).setLevel( logging.DEBUG )
+
+    unittest.main()
