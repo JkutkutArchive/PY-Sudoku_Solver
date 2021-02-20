d1aef40a214ecd1a6d97b0dfed716bbc42db944f
jkutkut <jorgeregonzalez@gmail.com>
2021-02-20 17:57:50 +0100

    sudoku.print test made

--- testing/sudoku_testing.py
+++ testing/sudoku_testing.py
@@ -24,32 +24,30 @@ class TestStringMethods(unittest.TestCase):
     #     self.assertTrue(False)
 
     def test_printSudokuTest(self):
-        # sudoku.fillBoard(input.easy())
-        # sudoku.fillBoard(input.medium())
-        # sudoku.print()
-        
-        logging.basicConfig()
-        log = logging.getLogger("LOG")
-        log.warning("Hola")
-        # log = logging.getLogger("TestStringMethods.printSudokuTest")
-        # log.debug("debug message")
-        
+        self.sudoku.fillBoard(input.easy())
+        self.maxDiff = None # To compare the whole output
 
-#         self.assertEqual(captured.out,"  C 0 1 2   3 4 5   6 7 8 \
-# R +-----------------------+\
-# 0 | 9 8 4 | 0 3 1 | 0 7 2 |\
-# 1 | 6 1 0 | 0 0 7 | 0 0 0 |\
-# 2 | 2 5 7 | 0 0 9 | 8 0 0 |\
-#   +-------+-------+-------+\
-# 3 | 3 0 0 | 0 6 0 | 0 1 0 |\
-# 4 | 0 0 0 | 3 7 0 | 9 2 0 |\
-# 5 | 0 0 9 | 0 0 5 | 0 0 0 |\
-#   +-------+-------+-------+\
-# 6 | 0 3 0 | 0 0 6 | 0 0 0 |\
-# 7 | 0 4 5 | 0 1 8 | 0 9 6 |\
-# 8 | 1 9 6 | 7 0 0 | 2 8 0 |\
-#   +-----------------------+")
-        self.assertEqual(1,2)
+        espected = \
+"  C 0 1 2   3 4 5   6 7 8 \n\
+R +-------+-------+-------+\n\
+0 | 9 8 4 | 0 3 1 | 0 7 2 |\n\
+1 | 6 1 0 | 0 0 7 | 0 0 0 |\n\
+2 | 2 5 7 | 0 0 9 | 8 0 0 |\n\
+  +-------+-------+-------+\n\
+3 | 3 0 0 | 0 6 0 | 0 1 0 |\n\
+4 | 0 0 0 | 3 7 0 | 9 2 0 |\n\
+5 | 0 0 9 | 0 0 5 | 0 0 0 |\n\
+  +-------+-------+-------+\n\
+6 | 0 3 0 | 0 0 6 | 0 0 0 |\n\
+7 | 0 4 5 | 0 1 8 | 0 9 6 |\n\
+8 | 1 9 6 | 7 0 0 | 2 8 0 |\n\
+  +-------+-------+-------+"
+
+        self.assertEqual(self.sudoku.print(returnAsString=True), espected)
+
+        self.sudoku.fillBoard(input.medium())
+        self.assertEqual(self.sudoku.print(arr=input.easy(), returnAsString=True), espected)
+        self.assertIsNone(self.sudoku.print())
 
 if __name__ == '__main__':
   print("Testing...")
