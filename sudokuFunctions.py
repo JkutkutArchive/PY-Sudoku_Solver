2c0f6622fcf77554efa337a989fe3d0a2dc1ada4
jkutkut <jorgeregonzalez@gmail.com>
2021-02-20 18:55:26 +0100

    sudoku.validSolution fully implemented

--- sudokuFunctions.py
+++ sudokuFunctions.py
@@ -1,35 +1,3 @@
-def checkSol(arr):
-    '''
-    Check if the given sudoku is a valid solution
-
-    Given a 9x9 Cell list, check if it forms a valid sudoku.
-
-    - arr (list): 9x9 Cell list
-
-    Returns:
-    boolean: whenever the input forms a valid sudoku
-
-    Note: See the Cell class for more information about who this function works.
-    '''
-    for i in range(0, 9, 3):#3 by 3:
-        for j in range(0, 9, 3):
-            suma = 0
-            ele = []
-            for k in range(3):
-                for l in range(3):
-                    suma = suma + arr[k + i][l + j].getValue()
-                    ele = ele + [arr[k + i][l + j].getValue()]
-            if(suma != 45):
-                return False
-    for l in range(9):#lines:
-        solCc = 0
-        solCr = 0
-        for i in range(9):
-            solCc = solCc + arr[i][l].getValue()
-            solCr = solCr + arr[l][i].getValue()
-        if solCc != 45 or solCr != 45:
-            return False
-    return True
 
 
 def sudokuSolution(data, solutions):
