e59d877016dcb975643886c69fe7686d0d19ac1d
jkutkut <jorgeregonzalez@gmail.com>
2021-02-22 13:18:19 +0100

    test for dataSudoku implemented

--- testing/dataSudoku_testing.py
+++ testing/dataSudoku_testing.py
@@ -14,40 +14,61 @@ import input
 
 class TestStringMethods(unittest.TestCase):
     def setUp(self):
-        self.data = dataSudoku.DataSudoku(dataSudoku.TypeHandler().therefore(), cell.Cell(0, 0), None)
         self.typeHandler = dataSudoku.TypeHandler()
+        self.cell = cell.Cell(0, 0)
+        self.data = dataSudoku.DataSudoku(self.typeHandler.therefore(), self.cell, None)
     
     def test_typeHandler(self):
         sw = self.typeHandler.switcher
         ignore = self.typeHandler.methodsToIgnore
         for i in range(len(sw)):
             self.assertFalse(all([sw[i] == skipM for skipM in ignore]))
+        
+        errorNotFound = "Data type not found"
+        for i in range(len(sw)):
+            self.assertTrue(self.typeHandler.validType(i))
+            self.assertTrue(self.typeHandler.typeConversor(i) != errorNotFound)
+        self.assertEqual(self.typeHandler.typeConversor(1 + len(sw)), errorNotFound)
+        self.assertEqual(self.typeHandler.typeConversor(-1), errorNotFound)
 
     
-    def test_initdata(self):
-        self.assertTrue(True)
+    def test_initdata(self):        
+        testInvalid = [
+            ["", self.cell, []],
+            [None, self.cell, []],
+            [100, self.cell, []],
+            [-1, self.cell, []],
+            [self.typeHandler.therefore(), None, []],
+            [self.typeHandler.therefore(), {}, []],
+        ]
+        exceptions = [
+            "DataType not valid",
+            "DataType not valid",
+            "DataType not valid",
+            "DataType not valid",
+            "The cell adressed is not a Cell",
+            "The cell adressed is not a Cell"
+        ]
+
+        testValid = [
+            [self.typeHandler.therefore(), self.cell, []],
+            [1, self.cell, []]
+        ]
+
+        for t in range(len(testInvalid)):
+            with self.assertRaises(Exception) as context:
+                dataSudoku.DataSudoku(*testInvalid[t])
+            self.assertTrue(exceptions[t] in str(context.exception))
         
-        # testInvalid = [
-        #     [None, None, None],
-        #     ["", cell.Cell(0,0), []],
-        #     [dataSudoku.TypeHandler.therefore(), cell.Cell(0, 0), []],
-        # ]
-        # exceptions = [
-        #     "DataType not valid",
-        #     "DataType not valid",
-        #     "The cell adressed is not a Cell"
-        # ]
+        for t in range(len(testValid)):
+            try:
+                dataSudoku.DataSudoku(*testValid[t])
+            except:
+                self.assertFalse(True)
+            self.assertTrue(True)
+        
+        
 
-        # testValid = [
-        #     [100, cell.Cell(0,0), None],
-        #     [1, cell.Cell(0,0), []]
-        # ]
-
-        # for t in range(len(testInvalid)):
-        #     with self.assertRaises(Exception) as context:
-        #         dataSudoku.DataSudoku(*testInvalid[t])
-            
-        #     self.assertTrue(exceptions[t] in str(context.exception))
 
 
 
