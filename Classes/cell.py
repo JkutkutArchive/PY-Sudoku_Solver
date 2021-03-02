18fca75892464e54dea77ce1b52e013f8447d638
jkutkut <jorgeregonzalez@gmail.com>
2021-03-02 17:01:47 +0100

    Now the cell class checks the format of the value adressed

--- Classes/cell.py
+++ Classes/cell.py
@@ -144,10 +144,18 @@ class Cell():
 
     def removePosVal(self, value):
         '''
-        Removes the current value addresed.
+        Removes the current value(s) addresed.
 
-        value (int): Value that this cell can no loger be.
+        value (int|set): Value(s) that this cell can no loger be.
         '''
+        if type(value) is int:
+            value = set([value])
+        if not type(value) is set:
+            # See how value is used on the method of the set object.
+            # Therefore, there's no real need to check if the content
+            #  of the set is correct or not.
+            raise Exception("The input must be a integer or a set of integers")
+
         self.getPosVal().discard(value) # this cell can no longer be the value "value"
         # for p in self.pairs: # for each pair in this cell (format of p: tuple(cell, value))
         #     if p[1] == value: # If there is a pair with this value => the mate should be this value
@@ -281,90 +289,92 @@ class Cell():
 
     # ******    DATA:    ******
     def addData(self, *dataArr): # Add data. If already added, do not duplicate the info
-        if dataArr not in self.data: # If this data not added yet
-            key = dataArr[0] # This first element represents the type of data
-            if "basic" in key: # If key is the type "basic": row, col or 3by3
-                for d in self.data: # Search for it
-                    if key == d[0]: # If data on d has exacly the same data type
-                        d[1].extend(dataArr[1]) # Update the previous data (Basic: row, col, 3by3)
-                        return # end Execution
+        # if dataArr not in self.data: # If this data not added yet
+        #     key = dataArr[0] # This first element represents the type of data
+        #     if "basic" in key: # If key is the type "basic": row, col or 3by3
+        #         for d in self.data: # Search for it
+        #             if key == d[0]: # If data on d has exacly the same data type
+        #                 d[1].extend(dataArr[1]) # Update the previous data (Basic: row, col, 3by3)
+        #                 return # end Execution
             
-            elif "cell" in key: # "pair one cell" is eq to: "pair row cell" and "pair col cell"
-                for d in self.data:
-                    if "cell" in d[0] and dataArr[1:] == d[1:]: # If the data entered now has already been added
-                        return # Do not added
-            self.data.append(dataArr) # If not founded or not basic, add it as new data
+        #     elif "cell" in key: # "pair one cell" is eq to: "pair row cell" and "pair col cell"
+        #         for d in self.data:
+        #             if "cell" in d[0] and dataArr[1:] == d[1:]: # If the data entered now has already been added
+        #                 return # Do not added
+        #     self.data.append(dataArr) # If not founded or not basic, add it as new data
+        return
 
     def dataToText(self): # Return a array of strings with the data ready to be red.
-        s = ["Let's focus on the cell on the position " + str(self.getPos())] # Start by giving the position of the cell
-        for d in self.data: # For each data piece stored on the array
-            key = d[0] # Type of data on this piece d
-            dataToAdd = "\n\n" # Here the data of this piece will be stored based on the key (then added to s)
-            if "therefore" == key: # Format: ["therefore"]
-                dataToAdd = "Therefore, the value of this cell is " + str(self.value)
-            elif "basic" in key: # Format: ["basic <TYPE>", <VALUE>]
-                tipo = key[6:] # only enter the <TYPE>
-                dataToAdd = "If we look at the " + tipo + " on this cell, this cell can not be " + str(d[1]) + "."
-            elif "unique" in key: # Format: ["unique <TYPE>", <VALUE>]
-                tipo = key[6:] # only enter the <TYPE>
-                dataToAdd = "If we look at the " + tipo + " containing this cell, we know that this cell should be " + str(d[1]) + " because no one this " + tipo + " can be this value."
-            elif "pairs" in key: # Format: ["pairs (two) <TYPE>", MATECELL, <VALUE>]
-                if "two" in key:
-                    dataToAdd = "If we take a look, this and the " + str(d[1].getPos()) + " cell are eather " + str(d[2]) + ". Both cells can only be these values."
-                # elif "one" in key or "row" in key or "col" in key: # If type of pair: 3by3, row or col
-                else: # If type of pair: 3by3, row or col
-                    if "val" in key:
-                        dataToAdd = "Having on mind that one of the cells " + str(d[1].getPos()) + " and " + str(d[2].getPos()) + " is a " + str(d[3]) + ", this cell can not be " + str(d[3]) + "."
-                    elif "cell" in key:
-                        dataToAdd = "This cell and " + str(d[1].getPos()) + " are linked. Value " + str(d[2]) + " is on one of these 2 cells."
+        # s = ["Let's focus on the cell on the position " + str(self.getPos())] # Start by giving the position of the cell
+        # for d in self.data: # For each data piece stored on the array
+        #     key = d[0] # Type of data on this piece d
+        #     dataToAdd = "\n\n" # Here the data of this piece will be stored based on the key (then added to s)
+        #     if "therefore" == key: # Format: ["therefore"]
+        #         dataToAdd = "Therefore, the value of this cell is " + str(self.value)
+        #     elif "basic" in key: # Format: ["basic <TYPE>", <VALUE>]
+        #         tipo = key[6:] # only enter the <TYPE>
+        #         dataToAdd = "If we look at the " + tipo + " on this cell, this cell can not be " + str(d[1]) + "."
+        #     elif "unique" in key: # Format: ["unique <TYPE>", <VALUE>]
+        #         tipo = key[6:] # only enter the <TYPE>
+        #         dataToAdd = "If we look at the " + tipo + " containing this cell, we know that this cell should be " + str(d[1]) + " because no one this " + tipo + " can be this value."
+        #     elif "pairs" in key: # Format: ["pairs (two) <TYPE>", MATECELL, <VALUE>]
+        #         if "two" in key:
+        #             dataToAdd = "If we take a look, this and the " + str(d[1].getPos()) + " cell are eather " + str(d[2]) + ". Both cells can only be these values."
+        #         # elif "one" in key or "row" in key or "col" in key: # If type of pair: 3by3, row or col
+        #         else: # If type of pair: 3by3, row or col
+        #             if "val" in key:
+        #                 dataToAdd = "Having on mind that one of the cells " + str(d[1].getPos()) + " and " + str(d[2].getPos()) + " is a " + str(d[3]) + ", this cell can not be " + str(d[3]) + "."
+        #             elif "cell" in key:
+        #                 dataToAdd = "This cell and " + str(d[1].getPos()) + " are linked. Value " + str(d[2]) + " is on one of these 2 cells."
                 
-            elif "delPair" in key: # Format: ["delPair <TYPE> value", <MATECELL.pos()>, <VALUE>]
-                if "remove" in key:
-                    dataToAdd = "The cell " + str(d[1]) + " has now the value " + str(d[2]) + " and these cells are linked, so this cell can not be " + str(d[2])
-                elif "set" in key:
-                    dataToAdd = "The cell " + str(d[1]) + " is no longer " + str(d[2]) + " and these cells were linked, so the value of this cell is " + str(d[2])
+        #     elif "delPair" in key: # Format: ["delPair <TYPE> value", <MATECELL.pos()>, <VALUE>]
+        #         if "remove" in key:
+        #             dataToAdd = "The cell " + str(d[1]) + " has now the value " + str(d[2]) + " and these cells are linked, so this cell can not be " + str(d[2])
+        #         elif "set" in key:
+        #             dataToAdd = "The cell " + str(d[1]) + " is no longer " + str(d[2]) + " and these cells were linked, so the value of this cell is " + str(d[2])
             
-            elif "X-Wing" in key: # Format: ["X-Wing <TYPE>", p1, p2, value]; <TYPE>=[row, col]
-                conclusion = ""
-                if "row" in key:
-                    conclusion = "column"
-                else:
-                    conclusion = "row"
-                dataToAdd = "Let's have a look at the following (X-Wing):" + \
-                    "\n  - Either the cell " + str(d[1][0].getPos()) + " or " + str(d[1][1].getPos()) + " has the value " + str(d[3]) + \
-                    "\n  - Either the cell " + str(d[2][0].getPos()) + " or " + str(d[2][1].getPos()) + " has the value " + str(d[3]) + \
-                    "\nAcording to this facts, none of the cells on the " + conclusion + "s " + str(d[1][0].y) + " or " + str(d[1][1].y) + " can be this value. Therefore, this cell can not be " + str(d[3])
-            elif "XY-Wing" in key: # Format: ["XY-Wing <TYPE>", [c1, c2, c3], [v1, v2, v3]]
-                dataToAdd = "Let's have a look at the following (XY-Wing):\n" + \
-                            "   Have a look to the cell " + str(d[1][0].getPos()) + ", and let's name it c1. " + \
-                            "The same way, let's name c2 = " + str(d[1][1].getPos()) + " and c3 = " + str(d[1][1].getPos()) + "\n" + \
-                            "   If we take a look at the possible values of the cells, they can only one of the following values:\n" + \
-                            "       c1: " + str((d[2][0], d[2][1])) + "; c2: " + str((d[2][0], d[2][2])) + "; c3: " + str((d[2][1], d[2][2])) + "\n" + \
-                            "   Furthermore, these cells are related with each other with the following values: \n" + \
-                            "       - c1 and c2 with the value " + str(d[2][0]) + "\n" + \
-                            "       - c1 and c3 with the value " + str(d[2][1]) + "\n" + \
-                            "       - c2 and c3 with the value " + str(d[2][2]) + "\n" + \
-                            "   On a first look, we can not determine the value of these cells. However, we can ensure that either c2 or c3 is a " + str(d[2][2]) + \
-                            "   For this reason, this cell can not be a " + str(d[2][2])
-            elif "Unique rectangle" in key:
-                dataToAdd = "Let's have a look at the following (Unique rectangle): \n" + \
-                            "   Let's name the following cells:\n" + \
-                            "       c1 = " + str(d[1][0].getPos()) + "; c2 = " + str(d[1][1].getPos()) + "; c3 = " + str(d[1][2].getPos()) + "\n" + \
-                            "   Having on mind these cells, they form a rectangle with this cell. Furthermore, all 4 cells have in common that they can only be " + str(d[2]) + \
-                            " (Except for the cell in consideration witch can be " + str(self.getPosVal()) + ").\n" + \
-                            "   For these reason, if c3 is one of the possible values, c1 should be the other value (and for this reason, the value of c2 should be the same as c3)." + \
-                            " This makes the cell in consideration not able to be neither of these values, and it should be other value different."
-            elif "Swordfish" in key: # Format: ["Swordfish <TYPE>", result, coordinates, v]
-                tipo = "col"
-                if "col" in key: # Col type
-                    tipo = "row"
-                dataToAdd = "Let's have a look at the following (Swordfish): \n" + \
-                            "If we look at the following cells: " + ", ".join([str(c.getPos()) for c in d[1]]) + "\n" + \
-                            "These cells all are pairs (divide them in groups of 2) that share the value " + str(d[3]) + ". Additionally, " + \
-                            "these cells are linked between each other (see how the coordinates are the same: if one cell has the same " + \
-                            "row coordinate with the next one, this cell is also connected with the following one with the column cell)\n" + \
-                            "For this reason, all the cells on the " + tipo + "s with the values " + ", ".join([str(c) for c in d[2]]) + \
-                            " (except the mencioned ones) can not be the value " + str(d[3]) + ".\nFor this reason, this cell can not be " + str(d[3])
-                print(dataToAdd)
-            s.append(dataToAdd) # Add it to the array with the rest
-        return s # Return all the data on text format
+        #     elif "X-Wing" in key: # Format: ["X-Wing <TYPE>", p1, p2, value]; <TYPE>=[row, col]
+        #         conclusion = ""
+        #         if "row" in key:
+        #             conclusion = "column"
+        #         else:
+        #             conclusion = "row"
+        #         dataToAdd = "Let's have a look at the following (X-Wing):" + \
+        #             "\n  - Either the cell " + str(d[1][0].getPos()) + " or " + str(d[1][1].getPos()) + " has the value " + str(d[3]) + \
+        #             "\n  - Either the cell " + str(d[2][0].getPos()) + " or " + str(d[2][1].getPos()) + " has the value " + str(d[3]) + \
+        #             "\nAcording to this facts, none of the cells on the " + conclusion + "s " + str(d[1][0].y) + " or " + str(d[1][1].y) + " can be this value. Therefore, this cell can not be " + str(d[3])
+        #     elif "XY-Wing" in key: # Format: ["XY-Wing <TYPE>", [c1, c2, c3], [v1, v2, v3]]
+        #         dataToAdd = "Let's have a look at the following (XY-Wing):\n" + \
+        #                     "   Have a look to the cell " + str(d[1][0].getPos()) + ", and let's name it c1. " + \
+        #                     "The same way, let's name c2 = " + str(d[1][1].getPos()) + " and c3 = " + str(d[1][1].getPos()) + "\n" + \
+        #                     "   If we take a look at the possible values of the cells, they can only one of the following values:\n" + \
+        #                     "       c1: " + str((d[2][0], d[2][1])) + "; c2: " + str((d[2][0], d[2][2])) + "; c3: " + str((d[2][1], d[2][2])) + "\n" + \
+        #                     "   Furthermore, these cells are related with each other with the following values: \n" + \
+        #                     "       - c1 and c2 with the value " + str(d[2][0]) + "\n" + \
+        #                     "       - c1 and c3 with the value " + str(d[2][1]) + "\n" + \
+        #                     "       - c2 and c3 with the value " + str(d[2][2]) + "\n" + \
+        #                     "   On a first look, we can not determine the value of these cells. However, we can ensure that either c2 or c3 is a " + str(d[2][2]) + \
+        #                     "   For this reason, this cell can not be a " + str(d[2][2])
+        #     elif "Unique rectangle" in key:
+        #         dataToAdd = "Let's have a look at the following (Unique rectangle): \n" + \
+        #                     "   Let's name the following cells:\n" + \
+        #                     "       c1 = " + str(d[1][0].getPos()) + "; c2 = " + str(d[1][1].getPos()) + "; c3 = " + str(d[1][2].getPos()) + "\n" + \
+        #                     "   Having on mind these cells, they form a rectangle with this cell. Furthermore, all 4 cells have in common that they can only be " + str(d[2]) + \
+        #                     " (Except for the cell in consideration witch can be " + str(self.getPosVal()) + ").\n" + \
+        #                     "   For these reason, if c3 is one of the possible values, c1 should be the other value (and for this reason, the value of c2 should be the same as c3)." + \
+        #                     " This makes the cell in consideration not able to be neither of these values, and it should be other value different."
+        #     elif "Swordfish" in key: # Format: ["Swordfish <TYPE>", result, coordinates, v]
+        #         tipo = "col"
+        #         if "col" in key: # Col type
+        #             tipo = "row"
+        #         dataToAdd = "Let's have a look at the following (Swordfish): \n" + \
+        #                     "If we look at the following cells: " + ", ".join([str(c.getPos()) for c in d[1]]) + "\n" + \
+        #                     "These cells all are pairs (divide them in groups of 2) that share the value " + str(d[3]) + ". Additionally, " + \
+        #                     "these cells are linked between each other (see how the coordinates are the same: if one cell has the same " + \
+        #                     "row coordinate with the next one, this cell is also connected with the following one with the column cell)\n" + \
+        #                     "For this reason, all the cells on the " + tipo + "s with the values " + ", ".join([str(c) for c in d[2]]) + \
+        #                     " (except the mencioned ones) can not be the value " + str(d[3]) + ".\nFor this reason, this cell can not be " + str(d[3])
+        #         print(dataToAdd)
+        #     s.append(dataToAdd) # Add it to the array with the rest
+        # return s # Return all the data on text format
+        return
