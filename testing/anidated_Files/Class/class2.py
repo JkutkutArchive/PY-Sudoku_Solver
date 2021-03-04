99f2642c1d813f5348d6d15caad9d155a91e6ab4
jkutkut <jorgeregonzalez@gmail.com>
2021-03-04 16:46:49 +0100

    deleted

--- testing/anidated_Files/Class/class2.py
+++
@@ -1,8 +0,0 @@
-class Class2():
-    def __init__(self):
-        print("created class2 object")
-    def tellTheType(self, obj):
-        if type(obj) is Class1:
-            return "Class1"
-        elif type(obj) is Class2:
-            return "same as Class2"
