26aad71880817f7f8c81d4061a718a015b585201
jkutkut <jorgeregonzalez@gmail.com>
2021-03-24 19:38:14 +0100

    Code migrated to new version

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 import tkinter as tk
 
-root = tk.Tk()
+# Variables:
+mainWidth, mainHeight = 500, 500
+squareSizeW, squareSizeH = mainWidth // 9, mainHeight // 9
+mouseX, mouseY = 0, 0
+
+# Functions:
+def motion(event):
+    '''
+    When the mouse is moved over the screen, this function is executed.
+    '''
+    global mouseX, mouseY
+    mouseX, mouseY = event.x, event.y # Store new values in a global variable
+
+def resize(event):
+    '''
+    When the screen is resized, this function is executed.
+    '''
+    global mainWidth, mainHeight, squareSizeW, squareSizeH
+    mainWidth, mainHeight = event.width, event.height # Store the current window size on global variables
+    squareSizeW, squareSizeH = mainWidth // 9, mainHeight // 9
+
+def leftMousePressed(event):
+    print("{} {}".format(mouseX, mouseY))
+
+
+# Setup
+root = tk.Tk(className='Sudoku solver')
 root.geometry("500x500")
+grid = tk.Grid()
+
+# Map functions to the events
+root.bind('<Motion>', motion)
+root.bind("<Configure>", resize)
+root.bind("<Button-1>", leftMousePressed)
 
 for i in range(9):
     tk.Grid.rowconfigure(root, index=i, weight=1)
@@ -14,109 +46,4 @@ for i in range(9):
         btn = buttons[i][j]
         btn.grid(row=i, column=j, sticky='nsew')
 
-root.mainloop()
-
-# import tkinter as tk
-
-# # Variables:
-# mainWidth, mainHeight = 500, 500
-# squareSizeW, squareSizeH = mainWidth // 9, mainHeight // 9
-# mouseX, mouseY = 0, 0
-
-# grid = None
-# cells = [[None for __ in range(9)] for _ in range(9)]
-# labels = [[None for __ in range(9)] for _ in range(9)]
-
-
-
-# # Setup
-
-# window = tk.Tk(className='Sudoku solver')
-# window.geometry("500x500")
-
-
-# def motion(event):
-#     '''
-#     When the mouse is moved over the screen, this function is executed.
-#     '''
-#     global mouseX, mouseY
-#     mouseX, mouseY = event.x, event.y # Store new values in a global variable
-
-# def resize(event):
-#     '''
-#     When the screen is resized, this function is executed.
-#     '''
-#     print(event)
-#     if event.widget.widgetName == "toplevel":
-#         return
-
-    
-#     global mainWidth, mainHeight, squareSizeW, squareSizeH, cells, labels
-#     mainWidth, mainHeight = event.width, event.height # Store the current window size on global variables
-#     squareSizeW, squareSizeH = mainWidth // 9, mainHeight // 9
-
-#     # print("{}, {}".format(squareSizeW, squareSizeH))
-#     # if (cells[0][0] == None):
-#     #     return
-#     # for i in range(9):
-#     #     posX = i * squareSizeW
-#     #     for j in range(9):
-#     #         posY = j * squareSizeH
-#     #         cell = cells[i][j]
-#     #         # cell.config(width = squareSizeW)
-#     #         # cell.config(height = squareSizeH)
-#     #         cell.place(
-#     #             x = posX,
-#     #             y = posY
-#     #         )
-
-
-
-
-
-# def leftMousePressed(event):
-#     print("{} {}".format(mouseX, mouseY))
-
-# # squareSizeH = 5
-
-# colors = ["white", "black"]
-
-# # grid = tk.Frame(
-# #     window,
-# #     width = mainWidth,
-# #     height = mainHeight,
-# #     bg = "lightblue"
-# # )
-# # grid.pack_propagate(0) # Stops child widgets of label_frame from resizing it
-
-# for i in range(9):
-#     for j in range(9):
-#         cells[i][j] = tk.Frame(
-#             window,
-#             width = squareSizeW,
-#             height = squareSizeH,
-#             bg = colors[(9 * i + j) % 2]
-#         )
-#         label = tk.Label(
-#             cells[i][j],
-#             bg="white",
-#             fg="black",
-#             text="test",
-#             font=("Calibri",10)
-#         )
-#         label.pack()
-#         cells[i][j].place(
-#             x = i * squareSizeW,
-#             y = j * squareSizeH
-#         )
-
-#         cells[i][j].pack_propagate(0) # Stops child widgets of label_frame from resizing it
-
-
-
-# window.bind('<Motion>', motion)
-# window.bind("<Configure>", resize)
-# window.bind("<Button-1>", leftMousePressed)
-
-# print(".")
-# window.mainloop()
+root.mainloop() 
