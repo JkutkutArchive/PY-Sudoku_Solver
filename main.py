3d3d8fd1d1574ecdcedd61a873069f1b0d3323cd
jkutkut <jorgeregonzalez@gmail.com>
2021-03-24 19:33:26 +0100

    huge improvement: now cells resize

--- main.py
+++ main.py
@@ -1,62 +1,122 @@
 import tkinter as tk
 
-# Variables:
-mainWidth, mainHeight = 500, 500
-mouseX, mouseY = 0, 0
+root = tk.Tk()
+root.geometry("500x500")
 
+for i in range(9):
+    tk.Grid.rowconfigure(root, index=i, weight=1)
+    tk.Grid.columnconfigure(root, index=i, weight=1)
 
-window = tk.Tk(className='Sudoku solver')
-window.geometry("500x500")
-
-# greeting = tk.Label(text="Hello, Tkinter")
-# greeting.pack()
-
-
-def motion(event):
-    '''
-    When the mouse is moved over the screen, this function is executed.
-    '''
-    global mouseX, mouseY
-    mouseX, mouseY = event.x, event.y # Store new values in a global variable
-
-def resize(event):
-    '''
-    When the screen is resized, this function is executed.
-    '''
-    global mainWidth, mainHeight
-    mainWidth, mainHeight = event.width, event.height # Store the current window size on global variables
-
-def leftMousePressed(event):
-    print("{} {}".format(mouseX, mouseY))
-
-window.bind('<Motion>', motion)
-window.bind("<Configure>", resize)
-window.bind("<Button-1>", leftMousePressed)
-
-
-# squareSizeW = mainWidth // 9
-squareSizeW = 10
-# squareSizeH = mainHeight // 9
-squareSizeH = 5
-
-colors = ["red", "blue"]
-
-labels = [[None for __ in range(9)] for _ in range(9)]
+buttons = [[tk.Button(root, text="btn ({}, {})".format(r, c)) for c in range(9)] for r in range(9)]
 
 for i in range(9):
     for j in range(9):
-        # frame1 = tk.Frame(master=window, width=squareSizeW, height=squareSizeH, bg=colors[(9 * i + j) % 2])
-        # frame1.pack(x=i*squareSizeW, y=j*squareSizeH)
+        btn = buttons[i][j]
+        btn.grid(row=i, column=j, sticky='nsew')
 
-        frame = tk.Frame(
-            master=window,
-            relief=tk.RAISED,
-            borderwidth=1
-        )
-        frame.grid(row=i, column=j)
-        labels[i][j] = tk.Label(master=frame, text=f"Row {i}\nColumn {j}", background=colors[(9 * i + j) % 2])
-        labels[i][j].config(width=squareSizeW)
-        labels[i][j].config(height=squareSizeH)
-        labels[i][j].pack()
+root.mainloop()
 
-window.mainloop()
+# import tkinter as tk
+
+# # Variables:
+# mainWidth, mainHeight = 500, 500
+# squareSizeW, squareSizeH = mainWidth // 9, mainHeight // 9
+# mouseX, mouseY = 0, 0
+
+# grid = None
+# cells = [[None for __ in range(9)] for _ in range(9)]
+# labels = [[None for __ in range(9)] for _ in range(9)]
+
+
+
+# # Setup
+
+# window = tk.Tk(className='Sudoku solver')
+# window.geometry("500x500")
+
+
+# def motion(event):
+#     '''
+#     When the mouse is moved over the screen, this function is executed.
+#     '''
+#     global mouseX, mouseY
+#     mouseX, mouseY = event.x, event.y # Store new values in a global variable
+
+# def resize(event):
+#     '''
+#     When the screen is resized, this function is executed.
+#     '''
+#     print(event)
+#     if event.widget.widgetName == "toplevel":
+#         return
+
+    
+#     global mainWidth, mainHeight, squareSizeW, squareSizeH, cells, labels
+#     mainWidth, mainHeight = event.width, event.height # Store the current window size on global variables
+#     squareSizeW, squareSizeH = mainWidth // 9, mainHeight // 9
+
+#     # print("{}, {}".format(squareSizeW, squareSizeH))
+#     # if (cells[0][0] == None):
+#     #     return
+#     # for i in range(9):
+#     #     posX = i * squareSizeW
+#     #     for j in range(9):
+#     #         posY = j * squareSizeH
+#     #         cell = cells[i][j]
+#     #         # cell.config(width = squareSizeW)
+#     #         # cell.config(height = squareSizeH)
+#     #         cell.place(
+#     #             x = posX,
+#     #             y = posY
+#     #         )
+
+
+
+
+
+# def leftMousePressed(event):
+#     print("{} {}".format(mouseX, mouseY))
+
+# # squareSizeH = 5
+
+# colors = ["white", "black"]
+
+# # grid = tk.Frame(
+# #     window,
+# #     width = mainWidth,
+# #     height = mainHeight,
+# #     bg = "lightblue"
+# # )
+# # grid.pack_propagate(0) # Stops child widgets of label_frame from resizing it
+
+# for i in range(9):
+#     for j in range(9):
+#         cells[i][j] = tk.Frame(
+#             window,
+#             width = squareSizeW,
+#             height = squareSizeH,
+#             bg = colors[(9 * i + j) % 2]
+#         )
+#         label = tk.Label(
+#             cells[i][j],
+#             bg="white",
+#             fg="black",
+#             text="test",
+#             font=("Calibri",10)
+#         )
+#         label.pack()
+#         cells[i][j].place(
+#             x = i * squareSizeW,
+#             y = j * squareSizeH
+#         )
+
+#         cells[i][j].pack_propagate(0) # Stops child widgets of label_frame from resizing it
+
+
+
+# window.bind('<Motion>', motion)
+# window.bind("<Configure>", resize)
+# window.bind("<Button-1>", leftMousePressed)
+
+# print(".")
+# window.mainloop()
