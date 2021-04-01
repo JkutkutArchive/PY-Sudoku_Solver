7dff67577ce8fedf2b20a8c95001535c2af38a4f
jkutkut <jorgeregonzalez@gmail.com>
2021-04-01 11:51:52 +0200

    typeHandler now based on static variables and methods. No constructor is needed

--- Classes/typeHandler.py
+++ Classes/typeHandler.py
@@ -2,32 +2,38 @@ class TypeHandler():
     '''
     This class enables to control the type of data asociated with a discovery on a sudoku cell
     '''
-    def __init__(self):
-        self.switcher = [ # Posible methods (part 1/2)
-            "therefore",
-            "basic",
-            "unique",
-            "pairs",
-            "Pairs_two",
-            "delPair_set_value",
-            "delPair_remove_value",
-            "xWing",
-            "xyWing",
-            "UniqueRectangle",
-            "swordfish"
-        ]
-        for i in range(len(self.switcher)): # For each possible method (part 1)
-            exec("self." + self.switcher[i] + " = lambda: " + str(i)) # Create the method
-        
-        self.subSwitcher = [ # Possible methods (part 2/2)
-            "row",
-            "col",
-            "3by3"
-        ]
-        for i in range(len(self.subSwitcher)): # For each subMethod
-            exec("self.sub" + self.subSwitcher[i] + " = lambda: " + str((i + 1) * 0.25)) # cretate it
 
-    def validType(self, typeData) -> bool:
+    # Lambda functions
+    switcher = [ # Posible methods (part 1/2 -> Type)
+        "therefore",
+        "basic",
+        "unique",
+        "pairs",
+        "Pairs_two",
+        "delPair_set_value",
+        "delPair_remove_value",
+        "xWing",
+        "xyWing",
+        "UniqueRectangle",
+        "swordfish"
+    ]
+
+    for i in range(len(switcher)): # For each possible method (part 1)
+            exec(switcher[i] + " = lambda: " + str(i)) # Create the method
+
+    subSwitcher = [ # Possible methods (part 2/2 -> subType)
+        "row",
+        "col",
+        "3by3"
+    ]
+
+    for i in range(len(subSwitcher)): # For each subMethod
+        exec("sub" + subSwitcher[i] + " = lambda: " + str((i + 1) * 0.25)) # cretate it
+
+    # Methods
+
+    @classmethod
+    def validType(cls, typeData) -> bool:
         '''
         Check whenever the input is a valid type of this class
         
@@ -38,11 +44,12 @@ class TypeHandler():
         
         if not type(typeData) is int and not type(typeData) is float or \
             typeData < 0 or \
-            typeData > len(self.switcher): 
+            typeData > len(cls.switcher): 
             return False
         return True
     
-    def typeConversor(self, t) -> str or int:
+    @classmethod
+    def typeConversor(cls, t) -> str or int:
         '''
         Transforms the input to the equivalent type (str -> int and int -> str).
 
@@ -53,21 +60,21 @@ class TypeHandler():
         (str|int): equivalent type.
         '''
         if type(t) is str: # If string given -> output a int
-            for i in range(len(self.switcher)): # For each possible type
-                if self.switcher[i] in t: # If type found
+            for i in range(len(cls.switcher)): # For each possible type
+                if cls.switcher[i] in t: # If type found
                     subType = 0
-                    for subT in range(len(self.subSwitcher)): # For each possible subtype
-                        if self.subSwitcher[subT] in t: # If subtype found
+                    for subT in range(len(cls.subSwitcher)): # For each possible subtype
+                        if cls.subSwitcher[subT] in t: # If subtype found
                             subType = (subT + 1) * 0.25
                     return i + subType # Return int type
             return "Data type not found" # If not found, return this
 
-        elif self.validType(t): # If valid integer given -> output str
+        elif cls.validType(t): # If valid integer given -> output str
             subType = ""
             if t % 1 != 0: # If decimal value, there is a subtype required
                 indexSubType = int((t % 1) // 0.25) - 1
                 t = int(t // 1)
-                subType = " " + self.subSwitcher[indexSubType] # If subtype, it will follow this syntax: "<type> <subtype>"
-            return self.switcher[t] + subType # Return the type
+                subType = " " + cls.subSwitcher[indexSubType] # If subtype, it will follow this syntax: "<type> <subtype>"
+            return cls.switcher[t] + subType # Return the type
         else:    
             return "The input is not valid" # If not valid input, return this
--- testing/typeHandler_testing.py
+++ testing/typeHandler_testing.py
@@ -14,39 +14,39 @@ import input
 
 class TestStringMethods(unittest.TestCase):
     def setUp(self):
-        self.typeHandler = TypeHandler()
+        # self.typeHandler = TypeHandler()
         self.cell = Cell(0, 0)
-        self.data = DataSudoku(self.typeHandler.therefore(), self.cell, None)
+        self.data = DataSudoku(TypeHandler.therefore(), self.cell, None)
     
     def test_typesExist(self):
-        sw = self.typeHandler.switcher
+        sw = TypeHandler.switcher
         
         errorNotFound = "Data type not found"
         notValidInput = "The input is not valid"
         for i in range(len(sw)):
-            self.assertTrue(self.typeHandler.validType(i))
-            self.assertTrue(self.typeHandler.typeConversor(i) != errorNotFound)
+            self.assertTrue(TypeHandler.validType(i))
+            self.assertTrue(TypeHandler.typeConversor(i) != errorNotFound)
         
-        self.assertEqual(self.typeHandler.typeConversor(1 + len(sw)), notValidInput)
-        self.assertEqual(self.typeHandler.typeConversor(-1), notValidInput)
+        self.assertEqual(TypeHandler.typeConversor(1 + len(sw)), notValidInput)
+        self.assertEqual(TypeHandler.typeConversor(-1), notValidInput)
 
         for type in sw:
-            typeIndex = self.typeHandler.typeConversor(type) # int eq to type
-            self.assertTrue(self.typeHandler.validType(typeIndex))
-            self.assertTrue(type == self.typeHandler.typeConversor(typeIndex)) # check the conversor is circular
+            typeIndex = TypeHandler.typeConversor(type) # int eq to type
+            self.assertTrue(TypeHandler.validType(typeIndex))
+            self.assertTrue(type == TypeHandler.typeConversor(typeIndex)) # check the conversor is circular
 
             for subType in ["row", "col", "3by3"]:
-                typeIndex = self.typeHandler.typeConversor(type + " " + subType) # int eq to type+subtype
-                self.assertTrue(self.typeHandler.validType(typeIndex))
-                self.assertTrue(type + " " + subType == self.typeHandler.typeConversor(typeIndex))
+                typeIndex = TypeHandler.typeConversor(type + " " + subType) # int eq to type+subtype
+                self.assertTrue(TypeHandler.validType(typeIndex))
+                self.assertTrue(type + " " + subType == TypeHandler.typeConversor(typeIndex))
                 
     def test_methodsExist(self):
-        sw = self.typeHandler.switcher
-        subSw = self.typeHandler.subSwitcher
+        sw = TypeHandler.switcher
+        subSw = TypeHandler.subSwitcher
         for type in sw:
-            self.assertTrue(type in dir(self.typeHandler))
+            self.assertTrue(type in dir(TypeHandler))
         for subType in subSw:
-            self.assertTrue("sub"+subType in dir(self.typeHandler))
+            self.assertTrue("sub"+subType in dir(TypeHandler))
 
 
 
